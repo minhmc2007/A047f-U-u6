56f1a997164d18eea6e57568812d58ca6dd9b26c
minhmc2007 <quangminh21072010@gmail.com>
2025-08-16 11:36:02 +0700

    clean up config, add kernelSU and more

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Thu May 22 13:31:39 UTC 2025"
-#define LINUX_COMPILE_BY "codespace"
-#define LINUX_COMPILE_HOST "codespaces-b157c5"
-#define LINUX_COMPILER "Android (5484270 based on r353983c) clang version 9.0.3 (https://android.googlesource.com/toolchain/clang 745b335211bb9eadfa6aa6301f84715cee4b37c5) (https://android.googlesource.com/toolchain/llvm 60cf23e54e46c807513f7a36d0a7b777920b5881) (based on LLVM 9.0.3svn), GNU ld (binutils-2.27-bd24d23f) 2.27.0.20170315"
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "4.19.198blue-nethunter-g090e4f5a2e42-dirty"
